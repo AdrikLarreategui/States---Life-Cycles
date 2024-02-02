55480d94d76a775e9b2bd8acf6fc11002b3aa276
AdrikLarreategui <adrian15186@hotmail.com>
2024-02-02 20:45:09 +0100

    commit Ejercicio 3 hecho

--- States And Life Cycles/States And Life Cycles/src/App.jsx
+++ States And Life Cycles/States And Life Cycles/src/App.jsx
@@ -1,29 +1,50 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css'
 
-const Counter = () => {
-  const[count, setCount] = useState(0);
+// const Counter = () => {
+//   const[count, setCount] = useState(0);
 
-  const increment = () => {
-    setCount (count +1);
-  };
+//   const increment = () => {
+//     setCount (count +1);
+//   };
 
-  const decrement = () => {
-    setCount(count -1);
-  };
+//   const decrement = () => {
+//     setCount(count -1);
+//   };
 
-return (
-  <div>
-    <button onClick= {decrement}>Decrement</button>
-    <p>Contador: {count}</p>
-    <button onClick= {increment}>Increment</button>
-  </div>
-)
-}
-export default Counter
+// return (
+//   <div>
+//     <button onClick= {decrement}>Decrement</button>
+//     <p>Contador: {count}</p>
+//     <button onClick= {increment}>Increment</button>
+//   </div>
+// )
+// }
+// export default Counter
+
+const Greeting = (props) => {
+  const [message, setMessage] = useState ('Hola, Euralio');
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMessage('Hola Alfonsina');
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default Greeting;
+
 
 
 
